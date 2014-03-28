c37912da0045213cd599679d2e40ebdf475cbf2b
southernbear <xq5530303@gmail.com>
2014-03-28 17:58:50 +0800

    move rs232_xmit_msg_task to output.c

--- src/kernel.c
+++ src/kernel.c
@@ -22,6 +22,7 @@
 #include "romfs.h"
 #include "object-pool.h"
 #include "serial.h"
+#include "output.h"
 #include "stack-pool.h"
 #include "procfs.h"
 #include "shell.h"
@@ -29,32 +30,6 @@
 #ifdef USE_TASK_STAT_HOOK
 #include "task-stat-hook.h"
 #endif /* USE_TASK_STAT_HOOK */
-
-void rs232_xmit_msg_task()
-{
-    int fdout;
-    int fdin;
-    char str[100];
-    int curr_char;
-
-    fdout = open("/dev/tty0/out", 0);
-    fdin = mq_open("/tmp/mqueue/out", O_CREAT);
-    setpriority(0, PRIORITY_DEFAULT - 2);
-
-    while (1) {
-        /* Read from the queue.  Keep trying until a message is
-         * received.  This will block for a period of time (specified
-         * by portMAX_DELAY). */
-        read(fdin, str, 100);
-
-        /* Write each character of the message to the RS232 port. */
-        curr_char = 0;
-        while (str[curr_char] != '\0') {
-            write(fdout, &str[curr_char], 1);
-            curr_char++;
-        }
-    }
-}
 
 void first()
 {
