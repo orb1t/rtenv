6f6fa3f7a5105caf70d982720e9cdb9d9ac7ae41
southernbear <xq5530303@gmail.com>
2014-04-10 18:51:30 +0800

    regenerate rom image if files in data/rom0 update

--- mk/romfs.mk
+++ mk/romfs.mk
@@ -1,13 +1,15 @@
 ROMDIR = $(DATDIR)/rom0
 DAT += $(OUTDIR)/$(DATDIR)/rom0.o
 
+ROMDAT := $(shell find $(ROMDIR) -mindepth 1 2>/dev/null)
+
 $(OUTDIR)/$(ROMDIR).o: $(OUTDIR)/$(ROMDIR).bin
 	@mkdir -p $(dir $@)
 	@echo "    OBJCOPY "$@
 	@$(CROSS_COMPILE)objcopy -I binary -O elf32-littlearm -B arm \
 		--prefix-sections '.rom' $< $@
 
-$(OUTDIR)/$(ROMDIR).bin: $(ROMDIR) $(OUTDIR)/$(TOOLDIR)/mkromfs
+$(OUTDIR)/$(ROMDIR).bin: $(ROMDIR) $(ROMDAT) $(OUTDIR)/$(TOOLDIR)/mkromfs
 	@mkdir -p $(dir $@)
 	@echo "    MKROMFS "$@
 	@$(OUTDIR)/$(TOOLDIR)/mkromfs -d $< -o $@
